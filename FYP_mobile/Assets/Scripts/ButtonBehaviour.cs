d79aaede70ac7f4760fa3a1e28c792ba51dfeb74
yingying0912 <xiiaoying5201314@gmail.com>
2021-02-16 02:27:29 +0800

    PC Server 0.0.3
    + swap scene between 2 project
    + made client and server using mirror

--- FYP_mobile/Assets/Scripts/ButtonBehaviour.cs
+++ FYP_mobile/Assets/Scripts/ButtonBehaviour.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Mirror;
+
 
 public class ButtonBehaviour: MonoBehaviour
 {
     public NetworkManager networkManager;
+    public Text t_ip;
 
-    public void OnServer()
+    public void OnClient()
     {
-        networkManager.StartServer();
+        networkManager.networkAddress = t_ip.text.ToString();
+        networkManager.StartClient();
     }
 }
--- FYP_pc/Assets/Scripts/ButtonBehaviour.cs
+++ FYP_pc/Assets/Scripts/ButtonBehaviour.cs
@@ -1,18 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using Mirror;
-
 
 public class ButtonBehaviour: MonoBehaviour
 {
     public NetworkManager networkManager;
-    public Text t_ip;
 
-    public void OnClient()
+    public void OnServer()
     {
-        networkManager.networkAddress = t_ip.text.ToString();
-        networkManager.StartClient();
+        networkManager.StartServer();
     }
 }
